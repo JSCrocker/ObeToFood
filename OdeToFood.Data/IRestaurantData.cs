3dc17e4ec3c67a9619e1c1766ffcee9db6f19ce4
Jonathan Crocker <scrocker.jonathan@gmail.com>
2020-08-25 14:47:00 -0500

    Created the Restaurant Razor Page and created the logic to display the dummy data

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,33 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name = "Scotts Pizza", Location = "MaryLand", Cuisine = CuisineType.Italian },
+                new Restaurant { Id = 2, Name = "Los Molcajetes", Location = "Haltom City", Cuisine = CuisineType.Mexican },
+                new Restaurant { Id = 3, Name = " Indian Imports", Location = "Arlington", Cuisine = CuisineType.Indian },
+            };       
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in restaurants
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
---
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.VisualStudio.Web.CodeGeneration.Contracts.Messaging;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class ListModel : PageModel
+    {
+        private readonly IRestaurantData restaurantData;
+
+        public string Message { get; set; }
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+
+        public ListModel(IRestaurantData restaurantData)
+        {
+            this.restaurantData = restaurantData;
+        }
+   
+        public void OnGet()
+        {
+            Restaurants = restaurantData.GetAll();
+        }
+    }
+}
