8513f66118545eb163be3484972bf0f35772fe49
Jonathan Crocker <scrocker.jonathan@gmail.com>
2020-09-03 09:51:44 -0500

    Wire up the User Interface
    Update the view of the Restaurant page. Edit the layout and crated partial views

---
+++ OdeToFood/View/RestaurantCount.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.View
+{
+    public class RestaurantCount : ViewComponent
+    {
+        private readonly IRestaurantData restaurantData;
+        public RestaurantCount(IRestaurantData restaurantData)
+        {
+            this.restaurantData = restaurantData;
+        }       
+
+        public IViewComponentResult Invoke()
+        {
+            var count = restaurantData.GetCountOfRestaurant();
+
+            return View(count);
+        }
+    }
+}
